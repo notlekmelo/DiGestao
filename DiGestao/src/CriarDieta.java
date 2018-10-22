ffb002abadab13aa55752120c58b2c43c451a4b9
noltlekmelo <kelton.fonseca@sga.pucminas.br>
2018-10-22 08:36:40 -0300

    atualizações feitas na aula, usuario e cria dieta

--- DiGestao/src/CriarDieta.java
+++ DiGestao/src/CriarDieta.java
@@ -1,39 +1,78 @@
 
 public class CriarDieta {
-	private String tipoDieta, meta;
+	private String tipoDieta;
+	private float metaKg;
 	private String[] dosagemAlimentos = new String[15];
-	private int tempoDieta;
-	
+	private int tempoDietaMeses;
+
 	public void historicoUsuario() {
-		
+
 	}
-	
+
 	public void preencherAlimentosHist() {
 		//Neste método o usuário preencherá o que comeu e haverá o calculo do que falta comer.
 	}
-	
+
+	public void consultaParametros(Usuario anonimo) {
+		String[] vet = new String[15];
+		if(anonimo.getAtvFis().equals("Sedentário")) {
+			if(anonimo.getMeta().equals("Emagrecer")) {
+				
+				inicializar("Emagrecer",vet,3 ,74 );
+			}
+			if(anonimo.getMeta().equals("Engordar")) {
+
+			}
+			if(anonimo.getMeta().equals("Manter")) {
+
+			}
+		}
+		if(anonimo.getAtvFis().equals("Ativo")) {
+			if(anonimo.getMeta().equals("Emagrecer")) {
+
+			}
+			if(anonimo.getMeta().equals("Engordar")) {
+
+			}
+			if(anonimo.getMeta().equals("Manter")) {
+
+			}
+		}
+		if(anonimo.getAtvFis().equals("Atleta")) {
+			if(anonimo.getMeta().equals("Emagrecer")) {
+
+			}
+			if(anonimo.getMeta().equals("Engordar")) {
+
+			}
+			if(anonimo.getMeta().equals("Manter")) {
+
+			}
+		}
+	}
+
 	//Por enquanto a dieta não recebe qual alimento deve comer, somente as quantidades de cada tipo de nutriente. A Meta definida vai ser comparada
 	//com o peso que ela alcançou. a dosagem e um vetor q define qtd de cada tipo de nutriente.
-	public void inicializar(String tipoDieta, String[] dosagemAlimentos, int tempoDieta, String meta) {
-		this.tipoDieta = tipoDieta;
+	public void inicializar(String tipoDieta, String[] dosagemAlimentos, int tempoDieta, float meta) {
+		this.tipoDieta= tipoDieta;
 		this.dosagemAlimentos = dosagemAlimentos;
-		this.tempoDieta = tempoDieta;
-		this.meta = meta;
+		this.tempoDietaMeses = tempoDieta;
+		this.metaKg = meta;
 	}
 
 	private String getTipoDieta() {
 		return tipoDieta;
 	}
 
-	private String getMeta() {
-		return meta;
+	private float getMetaKg() {
+		return metaKg;
 	}
 
 	private String[] getDosagemAlimentos() {
 		return dosagemAlimentos;
 	}
 
-	private int getTempoDieta() {
-		return tempoDieta;
+	private int getTempoDietaMeses() {
+		return tempoDietaMeses;
 	}
 }
--- DiGestao/src/Usuario.java
+++ DiGestao/src/Usuario.java
@@ -1,18 +1,46 @@
 
 public class Usuario extends Pessoa {
-	private String atvFis;
-	
-	public Usuario(String nome, String login, String senha, int idade, long cpf, String atvFis) {
+	private String atvFis,meta;
+	private float pesoKG ,alturaMetros;
+
+	public Usuario(String nome, String login, String senha, int idade, long cpf) {
 		super(nome, login,senha,idade,cpf);
-		this.atvFis = atvFis;
 	}
 	
+	private void setAtvFis(String atvFis) {
+		this.atvFis = atvFis;
+	}
+
 	public void enviaDuvida(String assunto,String mensagem) {
 		
 	}
 	
+	public void setMeta(String meta) {
+		this.meta = meta;
+	}
+	
+	public String getMeta() {
+		return this.meta;
+	}
+		
 	public String getAtvFis() {
 		return atvFis;
+	}
+	
+	private float getPesoKG() {
+		return pesoKG;
+	}
+
+	private void setPesoKG(float pesoKG) {
+		this.pesoKG = pesoKG;
+	}
+
+	private float getAlturaMetros() {
+		return alturaMetros;
+	}
+
+	private void setAlturaMetros(float alturaMetros) {
+		this.alturaMetros = alturaMetros;
 	}
 	
 	public String consultaDados() {
